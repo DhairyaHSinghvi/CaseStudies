a6debe1747840f8f17e26d9552c7870ba6c8fc45
DhairyaHSinghvi <dhairya.singhvi@somaiya.edu>
2021-07-28 09:31:41 +0530

    GitOperations added

---
+++ CaseStudies/GitOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaseStudies
+{
+    class GitOperations
+    {
+        static void main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+            
+            Console.WriteLine("This is from Test branch and expecting conflict");
+        }
+    }
+}
