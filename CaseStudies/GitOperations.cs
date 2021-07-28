8be0346e894ff04ab3251b07a195409d69d27975
DhairyaHSinghvi <dhairya.singhvi@somaiya.edu>
2021-07-28 09:26:39 +0530

    Addition property updated

---
+++ CaseStudies/GitOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaseStudies
+{
+    class GitOperations
+    {
+        static void main(String [] args)
+        {
+            Console.WriteLine("Hello World");
+            int x = 20;
+            int y = 30;
+            int z = x + y;
+            Console.WriteLine(z);
+        }
+    }
+}
